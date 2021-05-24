ea4cac96381aea57f8e94868b67b7f2e661dac9f
Patrick <patway99@gmail.com>
2021-05-24 14:29:42 -0700

    rewrite-config-for-test

--- src/main/java/org/acme/factor/service/FactorService.java
+++ src/main/java/org/acme/factor/service/FactorService.java
@@ -6,6 +6,7 @@ import io.smallrye.mutiny.Uni;
 import javax.enterprise.context.ApplicationScoped;
 import java.time.Duration;
 import java.util.List;
+import java.util.concurrent.Executor;
 
 @ApplicationScoped
 public class FactorService {
@@ -28,7 +29,20 @@ public class FactorService {
     public Uni<List<String>> collectItems(int count, String name) {
         Multi<String> multi = greetings(count, name);
         Uni<List<String>> uni = multi.collectItems().asList();
+        Executor e = new Executor() {
+            @Override
+            public void execute(Runnable command) {
+            }
+        };
+        multi.subscribeOn(e);
+        uni.subscribeOn(e);
         return uni;
+    }
+
+    public Multi<String> test(int count, String name) {
+        return Multi.createFrom().ticks().every(Duration.ofMillis(1))
+                .onItem().transform(n -> String.format("hello %s - %d", name, n))
+                .transform().byFilteringItemsWith(u -> u.matches("bla"));
     }
 
 
