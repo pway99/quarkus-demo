d7795322fe961284744309a124cd7748ebd18b04
Aaron Gershman <aegershman@gmail.com>
2021-07-01 16:10:08 -0500

    report generator to demonstrate ResourcePath to Location

---
+++ src/main/java/org/acme/demo/misc/ReportGenerator.java
@@ -0,0 +1,17 @@
+package org.acme.demo.misc;
+
+import io.quarkus.qute.Template;
+import io.quarkus.qute.api.ResourcePath;
+import io.quarkus.scheduler.Scheduled;
+
+public class ReportGenerator {
+    @ResourcePath("reports/v1/report_01")
+    Template report;
+
+    @Scheduled(cron = "0 30 * * * ?")
+    void generate() {
+        String result = report
+                .data("samples", new Object())
+                .render();
+    }
+}
