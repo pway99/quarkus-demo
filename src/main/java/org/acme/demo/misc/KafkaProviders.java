5a81da98259c9bc6141148943d006b3a328e1694
Aaron Gershman <aegershman@gmail.com>
2021-07-06 16:09:53 -0500

    KafkaProviders

---
+++ src/main/java/org/acme/demo/misc/KafkaProviders.java
@@ -0,0 +1,13 @@
+package org.acme.demo.misc;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+import java.util.Map;
+
+@ApplicationScoped
+public class KafkaProviders {
+    @Inject
+    @Named("default-kafka-broker")
+    Map<String, Object> config;
+}
