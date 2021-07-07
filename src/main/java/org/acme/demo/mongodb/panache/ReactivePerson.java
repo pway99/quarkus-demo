687cb00f1d206c0808c76dd0893ba4e13cdaa479
Aaron Gershman <aegershman@gmail.com>
2021-07-07 14:12:39 -0500

    reactive mongodb panache variant

--- src/main/java/org/acme/demo/mongodb/panache/ReactivePerson.java
+++ src/main/java/org/acme/demo/mongodb/panache/ReactivePerson.java
@@ -2,27 +2,27 @@ package org.acme.demo.mongodb.panache;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import io.quarkus.mongodb.panache.MongoEntity;
-import io.quarkus.mongodb.panache.PanacheMongoEntity;
+import io.quarkus.mongodb.panache.reactive.ReactivePanacheMongoEntity;
+import io.smallrye.mutiny.Uni;
 import org.bson.codecs.pojo.annotations.BsonProperty;
 
 import java.time.LocalDate;
 import java.util.List;
 
+// https://github.com/quarkusio/quarkus/blob/0dd0cdc75f10aefe7a3565b183f42f245184ab7a/docs/src/main/asciidoc/mongodb-panache.adoc#reactive-entities-and-repositories
 @MongoEntity(collection = "ThePerson")
-public class Person extends PanacheMongoEntity {
+public class ReactivePerson extends ReactivePanacheMongoEntity {
     @JsonProperty
     public String name;
-    // will be persisted as a 'birth' field in MongoDB
     @BsonProperty("birth")
     public LocalDate birthDate;
     public Status status;
 
-    // entity methods
-    public static Person findByName(String name) {
+    public static Uni<ReactivePerson> findByName(String name) {
         return find("name", name).firstResult();
     }
 
-    public static List<Person> findAlive() {
+    public static Uni<List<ReactivePerson>> findAlive() {
         return list("status", Status.LIVING);
     }
 
--- src/main/java/org/acme/demo/mongodb/panache/Resource.java
+++ src/main/java/org/acme/demo/mongodb/panache/Resource.java
@@ -1,55 +1,70 @@
 package org.acme.demo.mongodb.panache;
 
+import io.quarkus.mongodb.panache.reactive.ReactivePanacheMongoEntityBase;
+import io.smallrye.mutiny.Multi;
+import io.smallrye.mutiny.Uni;
 import org.bson.types.ObjectId;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.Response;
 import java.util.List;
+import java.util.Optional;
 
+// https://github.com/quarkusio/quarkus/blob/0dd0cdc75f10aefe7a3565b183f42f245184ab7a/docs/src/main/asciidoc/mongodb-panache.adoc#reactive-entities-and-repositories
 @Path("/persons")
 @Consumes("application/json")
 @Produces("application/json")
 public class Resource {
 
     @GET
-    public List<Person> list() {
-        return Person.listAll();
+    public static Multi<ReactivePerson> streamPersons() {
+        return ReactivePerson.streamAll();
+    }
+
+    @GET
+    public static Uni<List<ReactivePerson>> list() {
+        Uni<List<ReactivePerson>> persons = ReactivePerson.listAll();
+        return persons;
     }
 
     @GET
     @Path("/{id}")
-    public Person get(@PathParam("id") String id) {
-        return Person.findById(new ObjectId(id));
+    public static Uni<ReactivePerson> get(@PathParam("id") String id) {
+        Uni<ReactivePerson> personById = ReactivePerson.findById(new ObjectId(id));
+        return personById;
     }
 
     @POST
-    public Response create(Person person) {
-        person.persist();
+    public static Response create(ReactivePerson person) {
+        Uni<Void> p = person.persist();
         return Response.status(201).build();
     }
 
     @PUT
     @Path("/{id}")
-    public void update(@PathParam("id") String id, Person person) {
-        person.update();
+    public static void update(@PathParam("id") String id, ReactivePerson person) {
+        Uni<Void> p = person.update();
     }
 
     @DELETE
     @Path("/{id}")
-    public void delete(@PathParam("id") String id) {
-        Person person = Person.findById(new ObjectId(id));
-        person.delete();
+    public static void delete(@PathParam("id") String id) {
+        Uni<Optional<ReactivePerson>> optional = ReactivePerson.findByIdOptional(id);
+        Uni<ReactivePerson> person = optional.map(o -> o.orElseThrow(NotFoundException::new));
+        person.onItem().transform(ReactivePanacheMongoEntityBase::delete);
     }
 
     @GET
     @Path("/search/{name}")
-    public Person search(@PathParam("name") String name) {
-        return Person.findByName(name);
+    public static Uni<ReactivePerson> search(@PathParam("name") String name) {
+        Uni<ReactivePerson> person = ReactivePerson.findByName(name);
+        return person;
     }
 
     @GET
     @Path("/count")
-    public Long count() {
-        return Person.count();
+    public static Uni<Long> count() {
+        Uni<Long> count = ReactivePerson.count();
+        return count;
     }
 }
